a4ea1e95d1b0901c616fac22f9f485861aae059d
Ojas Singh <ojas.singh02@gmail.com>
2025-12-02 13:49:37 -0500

    Add a linter I dont like

--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -1,15 +1,25 @@
+import { Button } from "@/components/ui/button";
+
 export function Hero() {
   return (
     <main className="grid h-screen grid-rows-12 place-content-center">
       <div className="row-start-4 mx-auto max-w-xl text-center">
-        <h1 className="text-5xl">
+        <h1 className="mt-4 text-5xl">
           Signal is an AI assistant for revenue-first analytics
         </h1>
         <p className="mt-6 text-lg text-muted-foreground">
-          Beyond showing you data, Signal studies your users to provide
-          suggestions to get to product-market fit{" "}
+          Beyond showing you data in real-time, Signal studies your users to
+          provide suggestions to get to product-market fit{" "}
           <span className="text-primary">fast</span>.
         </p>
+        <div className="mt-6 flex place-content-center gap-2">
+          <Button className="rounded-full text-white">
+            Start 14-day free trial
+          </Button>
+          <Button className="rounded-full" variant="outline">
+            Demo
+          </Button>
+        </div>
       </div>
     </main>
   );
