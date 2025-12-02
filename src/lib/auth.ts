d211c11019ee1036652b8bbf8513ff983802b700
Ojas Singh <ojas.singh02@gmail.com>
2025-12-01 20:18:25 -0500

    Make auth stateless

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
+    baseURL: process.env.BETTER_AUTH_URL as string,
     socialProviders: { 
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID as string, 
@@ -11,9 +12,16 @@ export const auth = betterAuth({
             clientId: process.env.GOOGLE_CLIENT_ID as string, 
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string, 
         }, 
-    }, 
+    },
     session: {
-        freshAge: 60 * 5 
+        cookieCache: {
+            enabled: true,
+            maxAge: 7 * 24 * 60 * 60,
+        },
+    },
+    account: {
+        storeStateStrategy: "cookie",
+        storeAccountCookie: true,
     },
     plugins: [nextCookies()] 
 })
