65bffd7faedc4a8e07034e89d071b11dd760e64a
Ojas Singh <ojas.singh02@gmail.com>
2025-12-02 23:52:05 -0500

    Landing page improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,10 @@ import { Nav } from "@/components/landing-page/nav";
 
 export default function Home() {
   return (
-    <main className="mx-auto max-w-7xl border-x">
-      <Nav />
-      <div className="px-8">
+    <main className="relative flex justify-center border-x-0">
+      <div className="-z-10 absolute inset-0 bg-[repeating-linear-gradient(315deg,color-mix(in_oklab,var(--color-border),transparent_50%)_0,color-mix(in_oklab,var(--color-border),transparent_50%)_1px,transparent_0,transparent_50%)] bg-size-[10px_10px]" />
+      <div className="relative mx-auto w-full max-w-7xl border-x bg-background">
+        <Nav />
         <Hero />
       </div>
     </main>
--- src/components/landing-page/hero.tsx
+++ src/components/landing-page/hero.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 export function Hero() {
   return (
-    <div className="grid h-screen grid-rows-12 place-content-center">
+    <div className="grid h-screen grid-rows-12 place-content-center bg-[radial-gradient(circle_at_center,color-mix(in_oklab,var(--color-border)_75%,black)_1px,transparent_1px)] bg-size-[16px_16px]">
       <div className="row-start-4 mx-auto max-w-xl text-center">
         <h1 className="mt-4 font-medium text-5xl tracking-tight">
           Signal is an AI assistant for revenue analytics
@@ -13,12 +13,12 @@ export function Hero() {
         </p>
         <div className="mt-6 flex place-content-center gap-2">
           <Button
-            className="inset-shadow-primary inset-shadow-sm rounded-full text-white"
+            className="inset-shadow-primary inset-shadow-sm rounded-full text-white backdrop-blur-sm"
             variant="outline"
           >
             Start 14-day free trial
           </Button>
-          <Button className="rounded-full" variant="outline">
+          <Button className="rounded-full backdrop-blur-sm" variant="outline">
             Demo
           </Button>
         </div>
