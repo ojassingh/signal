340b03e3fb787aff08d490b48399ab57d5fcbc88
Ojas Singh <ojas.singh02@gmail.com>
2025-12-21 23:43:14 -0500

    Add login loading spinner

--- src/app/(auth)/components/auth-buttons.tsx
+++ src/app/(auth)/components/auth-buttons.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { Loader } from "lucide-react";
+import { useState } from "react";
 import { GitHubIcon, GoogleIcon } from "@/app/(auth)/components/auth-icons";
 import { Button } from "@/components/ui/button";
 import { signIn } from "@/lib/auth-client";
@@ -11,12 +13,17 @@ export function AuthButton({
   provider: "google" | "github";
   children: React.ReactNode;
 }) {
+  const [loading, setLoading] = useState(false);
   return (
     <Button
       className="w-full"
-      onClick={() => signIn.social({ provider, callbackURL: "/dashboard" })}
+      onClick={() => {
+        setLoading(true);
+        signIn.social({ provider, callbackURL: "/dashboard" });
+      }}
       variant="outline"
     >
+      {loading ? <Loader className="size-4 animate-spin" /> : null}
       {provider === "google" ? <GoogleIcon /> : <GitHubIcon />}
       {children}
     </Button>
